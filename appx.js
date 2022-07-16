7e6deb27266ee8ed004b5c9c34d3080ae0f0e852
Saul Zacarias <sezacarias@gmail.com>
2022-07-15 20:27:23 -0400

    Creation of a for bucle

--- appx.js
+++ appx.js
@@ -3,7 +3,17 @@ const originalstory = `Había una vez un enorme ${userImpunts [0]} que cazaba en
 
 Asimismo le indicó que si no le creía le acompañara: así vería como todos los animales huían atemorizados al verle llegar. El ${userImpunts [0]} así lo hizo, observando en efecto como a su paso los animales escapaban. Lo que no sabía era que esto no era debido a que estuvieran confirmando las palabras del ${userImpunts [4]} (algo que el ${userImpunts [0]} acabó por creer), sino que de hecho huían de la presencia del ${userImpunts [5]}.`;
 let userImpunts = [] ;
+const numberOfQuestions = 6;
+const questionArray = ["Ingrese un nombre para el protagonista" , "Ingrese un país" , "Ingrese un sustantivo (ejm. 'animal')" , "Ingrese un verbo (ejm. 'golpear'" , "Ingrese un nombre para el antagonista" , "Ingrese un verbo (ejm. 'golpear'" , "Ingrese un sustantivo (ejm. 'felino')"];
+for (let i = questionArray.length ; i >= 0 ; i--){
 
+
+
+
+
+
+    
+}
 /* Había una vez un enorme tigre que cazaba en los bosques de China. El poderoso animal se topó y empezó a atacar a un pequeño zorro, el cual ante el peligro únicamente tuvo como opción recurrir a la astucia. Así, el zorro le increpó y le indicó que no sabía hacerle daño puesto que él era el rey de los animales por designio del emperador del cielo.
 
 Asimismo le indicó que si no le creía le acompañara: así vería como todos los animales huían atemorizados al verle llegar. El tigre así lo hizo, observando en efecto como a su paso los animales escapaban. Lo que no sabía era que esto no era debido a que estuvieran confirmando las palabras del zorro (algo que el tigre acabó por creer), sino que de hecho huían de la presencia del felino.*/
