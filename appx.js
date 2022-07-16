484bda0eabe823ef6b5ec3cb4b8d2bcae512c999
Saul Zacarias <sezacarias@gmail.com>
2022-07-15 20:09:05 -0400

    Added mad-lib replacement variables to story

---
+++ appx.js
@@ -0,0 +1,9 @@
+"use strict";
+const originalstory = `Había una vez un enorme ${userImpunts [0]} que cazaba en los bosques de ${userImpunts [1]}. El poderoso ${userImpunts [2]} se topó y empezó a ${userImpunts [3]} a un pequeño ${userImpunts [4]}, el cual ante el peligro únicamente tuvo como opción recurrir a la astucia. Así, el ${userImpunts [4]} le increpó y le indicó que no sabía hacerle daño puesto que él era el rey de los animales por designio del emperador del cielo.
+
+Asimismo le indicó que si no le creía le acompañara: así vería como todos los animales huían atemorizados al verle llegar. El ${userImpunts [0]} así lo hizo, observando en efecto como a su paso los animales escapaban. Lo que no sabía era que esto no era debido a que estuvieran confirmando las palabras del ${userImpunts [4]} (algo que el ${userImpunts [0]} acabó por creer), sino que de hecho huían de la presencia del ${userImpunts [5]}.`;
+let userImpunts = [] ;
+
+/* Había una vez un enorme tigre que cazaba en los bosques de China. El poderoso animal se topó y empezó a atacar a un pequeño zorro, el cual ante el peligro únicamente tuvo como opción recurrir a la astucia. Así, el zorro le increpó y le indicó que no sabía hacerle daño puesto que él era el rey de los animales por designio del emperador del cielo.
+
+Asimismo le indicó que si no le creía le acompañara: así vería como todos los animales huían atemorizados al verle llegar. El tigre así lo hizo, observando en efecto como a su paso los animales escapaban. Lo que no sabía era que esto no era debido a que estuvieran confirmando las palabras del zorro (algo que el tigre acabó por creer), sino que de hecho huían de la presencia del felino.*/
