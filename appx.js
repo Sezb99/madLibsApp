fb4dda2ffe738254fa3988291fe5e9eeaa45e0ef
Saul Zacarias <sezacarias@gmail.com>
2022-07-15 20:43:22 -0400

    Prueba de funcionamiento exitosa

--- appx.js
+++ appx.js
@@ -1,19 +1,20 @@
 "use strict";
+let userImpunts = [];
 const originalstory = `Había una vez un enorme ${userImpunts [0]} que cazaba en los bosques de ${userImpunts [1]}. El poderoso ${userImpunts [2]} se topó y empezó a ${userImpunts [3]} a un pequeño ${userImpunts [4]}, el cual ante el peligro únicamente tuvo como opción recurrir a la astucia. Así, el ${userImpunts [4]} le increpó y le indicó que no sabía hacerle daño puesto que él era el rey de los animales por designio del emperador del cielo.
 
 Asimismo le indicó que si no le creía le acompañara: así vería como todos los animales huían atemorizados al verle llegar. El ${userImpunts [0]} así lo hizo, observando en efecto como a su paso los animales escapaban. Lo que no sabía era que esto no era debido a que estuvieran confirmando las palabras del ${userImpunts [4]} (algo que el ${userImpunts [0]} acabó por creer), sino que de hecho huían de la presencia del ${userImpunts [5]}.`;
-let userImpunts = [] ;
 const numberOfQuestions = 6;
-const questionArray = ["Ingrese un nombre para el protagonista" , "Ingrese un país" , "Ingrese un sustantivo (ejm. 'animal')" , "Ingrese un verbo (ejm. 'golpear'" , "Ingrese un nombre para el antagonista" , "Ingrese un verbo (ejm. 'golpear'" , "Ingrese un sustantivo (ejm. 'felino')"];
+const questionArray = ["Ingrese un nombre para el protagonista" , "Ingrese un país" , "Ingrese un sustantivo (ejm. 'animal')" , "Ingrese un verbo (ejm. 'golpear'" , "Ingrese un nombre para el antagonista" , "Ingrese un sustantivo (ejm. 'felino')"];
 let questionCounter = 0;
-for (let i = questionArray.length ; i >= 0 ; i--){
+for (let i = questionArray.length ; i > 0 ; i--){
+
+console.log(questionArray[questionCounter]);
 
 
 
 
 
-
-questionArray++;
+questionCounter++;
 }
 /* Había una vez un enorme tigre que cazaba en los bosques de China. El poderoso animal se topó y empezó a atacar a un pequeño zorro, el cual ante el peligro únicamente tuvo como opción recurrir a la astucia. Así, el zorro le increpó y le indicó que no sabía hacerle daño puesto que él era el rey de los animales por designio del emperador del cielo.
 
